4f8ac3e640797a5a1878523c4138ac41b9a5a6d3
Duc Truong <minhduc9111999@gmail.com>
2024-01-03 19:14:13 +0700

    Add pairs, surround, comments utils config

---
+++ .config/nvim/lua/plugins/utils.lua
@@ -0,0 +1,10 @@
+return {
+  "echasnovski/mini.nvim",
+  version = "*",
+  config = function()
+    require("mini.pairs").setup()
+    require("mini.surround").setup()
+    require("mini.comment").setup()
+    require("mini.splitjoin").setup()
+  end,
+}
